b87a96fabcb8201de4541a0b3803d1853c84fca8
Tugrul Can <tugrulcansollu@gmail.com>
2017-04-29 00:59:17 +0300

    Update init-user-db.sh

--- postgres/init-user-db.sh
+++ postgres/init-user-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -e [ set -e]
+set -e [ set -e ]
 psql -d template1 -U postgres  <<-EOSQL
-CREATE USER ojs WITH PASSWORD 'ojs';
-CREATE DATABASE ojs;
-GRANT ALL PRIVILEGES ON DATABASE ojs to ojs;
+CREATE USER ojs WITH PASSWORD 'ojs' ;
+CREATE DATABASE ojs ;
+GRANT ALL PRIVILEGES ON DATABASE ojs TO ojs ;
 EOSQL
